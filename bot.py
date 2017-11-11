6a36d52f1b16ae2ab466a6bd4866c0a43cf8a4af
Rustem7 <30521089+Rustem7@users.noreply.github.com>
2017-11-12 00:46:34 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,17 +29,14 @@ class Bot:
         self._updater.dispatcher.add_handler(CommandHandler('rate', self._check_rate))
     
     @staticmethod
-    def main():
-        
-        parse(site('http://www.kino.kz/cinema.asp?cinemaid='+constants.krg))
-    if __name__ == '__main__':
-    main()
+     
+       
         
     
-    def _check_rate(bot, update):parse(site('http://www.kino.kz/cinema.asp?cinemaid='+constants.krg))
+    def _check_rate(bot, update):
         message = update.message
         
-        url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+        url = "http://www.kino.kz/cinema.asp?cinemaid='+constants.krg"
         
         response = requests.get(url)
         rate = response.json()['bpi']['USD']['rate_float']
@@ -47,26 +44,6 @@ class Bot:
         text = "Current Bitcoin rate - ${}".format(rate)
         bot.send_message(chat_id=message.chat_id, text=text)
         
-    def site(url):
-        coll = urllib.request.urlopen(url)
-        time.sleep(2)
-        return coll.read()
+       
     
-    def parse(html):
-        @bot.message_handler(content_types=['text'])
-        def handle_text(message):
-            if message.text == "Фильм":
-                lines = []
-                soup = BeautifulSoup(html, 'html.parser')
-                for s in soup.find_all('div', class_='detail_content'):
-                    for k in s.find_all('tr'):
-                        for b in k.find_all('strong'):
-                            z = b.text
-                            I = '🔴🎥|'
-                            lines.append(I + z + '|' + '\n' + 'Время сеанса:' + '\n' + '------------------' + '\n')
-                        for h in k.find_all('tr', class_='seance_active'):
-                            for p in h.findAll('td')[-10:1]:
-                                a = p.text[11:-5]
-                                lines.append('⏰' + a + '\n' + '------------------' + '\n')
-                                
-                bot.send_message(message.chat.id, '3323')
+    
