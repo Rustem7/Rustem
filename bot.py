10028c8914e644eaf7b7137d3d9faabca09d7cab
Rustem7 <30521089+Rustem7@users.noreply.github.com>
2017-11-12 00:04:40 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,22 +48,20 @@ class Bot:
         return coll.read()
     
     def parse(html):
-
-
-    @bot.message_handler(content_types=['text'])
-    def handle_text(message):
-
-        if message.text == "Фильм":
-            lines = []
-            soup = BeautifulSoup(html, 'html.parser')
-            for s in soup.find_all('div', class_='detail_content'):
-                for k in s.find_all('tr'):
-                    for b in k.find_all('strong'):
-                        z = b.text
-                        I = '🔴🎥|'
-                        lines.append(I + z + '|' + '\n' + 'Время сеанса:' + '\n' + '------------------' + '\n')
-                    for h in k.find_all('tr', class_='seance_active'):
-                        for p in h.findAll('td')[-10:1]:
-                            a = p.text[11:-5]
-                            lines.append('⏰' + a + '\n' + '------------------' + '\n')
-            bot.send_message(message.chat.id, ''.join(lines))
+        @bot.message_handler(content_types=['text'])
+        def handle_text(message):
+            if message.text == "Фильм":
+                lines = []
+                soup = BeautifulSoup(html, 'html.parser')
+                for s in soup.find_all('div', class_='detail_content'):
+                    for k in s.find_all('tr'):
+                        for b in k.find_all('strong'):
+                            z = b.text
+                            I = '🔴🎥|'
+                            lines.append(I + z + '|' + '\n' + 'Время сеанса:' + '\n' + '------------------' + '\n')
+                        for h in k.find_all('tr', class_='seance_active'):
+                            for p in h.findAll('td')[-10:1]:
+                                a = p.text[11:-5]
+                                lines.append('⏰' + a + '\n' + '------------------' + '\n')
+                                
+                 bot.send_message(message.chat.id, ''.join(lines))
