b650ad46715e28711e093c846665516e56bff37a
Rustem7 <30521089+Rustem7@users.noreply.github.com>
2017-11-11 17:33:24 +0600

    Update Poc.py

--- Poc.py
+++ Poc.py
@@ -1,24 +1,37 @@
 import constants
 import os
 import telebot
+from flask import Flask, request
 
-TOKEN = os.environ.get('TOKEN', '341519589:AAGsM9G8_0UHiMxRF2uUhXdootK8m086Yqo')
-PORT = int(os.environ.get('PORT', '5000'))
+server = Flask(__name__)
+TOKEN  = 'token'
+
 bot = telebot.TeleBot(constants.token)
 
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    if message.text == "Фильм":
-   
-        bot.send_message(message.chat.id, '66')
+port = int(os.environ.get("PORT", 5000))
 
-updater = Updater(TOKEN)
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://polar-inlet-33421.herokuapp.com/") #ссылку изменил
+    return "!", 200
 
-# add handlers
-updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_messages(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8")).message])
+    
+    
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_messages(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8")).message])
+    return "!", 200
 
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.setWebhook("https://polar-inlet-33421.herokuapp.com/" + TOKEN)
-updater.idle()
+@bot.message_handler()
+def start(message):
+    bot.send_message(message.chat.id, 'Hi') #вот эта часть кода исполняется два или три раза
+
+server.run(host='0.0.0.0', port=port)    
+    
+    
