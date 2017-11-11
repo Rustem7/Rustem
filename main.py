ff7794ddc66e99f3c7db604bd535235659f07a11
Rustem7 <30521089+Rustem7@users.noreply.github.com>
2017-11-11 23:48:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bot = Bot(token)
 
 
 def site(url):
-    coll = urllib.request.urlopen(http://www.kino.kz/cinema.asp?cinemaid='+constants.krg)
+    coll = urllib.request.urlopen('http://www.kino.kz/cinema.asp?cinemaid=50')
     time.sleep(2)
     return coll.read()
 bot.run()
