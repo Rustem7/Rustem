49d131ccbd9fcbfaf4be5093672fc6feb977562d
Rustem7 <30521089+Rustem7@users.noreply.github.com>
2017-11-11 23:42:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
-
+import urllib.request
 from bot import Bot
 
 
 token = os.environ.get("TOKEN")
 
 bot = Bot(token)
+
+parse(site('http://www.kino.kz/cinema.asp?cinemaid='+constants.krg))
+
+def site(url):
+    coll = urllib.request.urlopen(url)
+    time.sleep(2)
+    return coll.read()
 bot.run()
