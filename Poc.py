8afd2305bd57c73d7fe8712fba35acce2d85f310
Rustem7 <30521089+Rustem7@users.noreply.github.com>
2017-11-11 13:14:44 +0600

    Update Poc.py

--- Poc.py
+++ Poc.py
@@ -1,29 +1,27 @@
+import urllib.request
+from bs4 import BeautifulSoup
 import telebot
-import os
-from flask import Flask, request
+import time
+import constants
 
-bot = telebot.TeleBot('341519589:AAGsM9G8_0UHiMxRF2uUhXdootK8m086Yqo')
+bot = telebot.TeleBot("341519589:AAGsM9G8_0UHiMxRF2uUhXdootK8m086Yqo")
+@bot.message_handler(commands=['start', 'help'])
+def handle_start(m):
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('Фильм')
+    bot.send_message(m.chat.id, 'Привет',reply_markup=markup)
 
-server = Flask(__name__)
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if message.text == "Фильм":
+        bot.send_message(message.chat.id, 'ddddd')
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
 
-@server.route("/bot", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://git.heroku.com/polar-inlet-33421.git")
-    return "!", 200
+def main():
+  # parse(site('http://www.kino.kz/cinema.asp?cinemaid=50'))
+  if __name__ == '__main__':
+      main()
 
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-server = Flask(__name__)
+bot.polling(none_stop=True)
