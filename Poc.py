733e2d6f1252b015de0f2958fdca912eaa35ee01
Rustem7 <30521089+Rustem7@users.noreply.github.com>
2017-11-11 14:32:45 +0600

    Update Poc.py

--- Poc.py
+++ Poc.py
@@ -1,29 +1,22 @@
-import telebot
+
 import os
-from flask import Flask, request
+from telegram.ext import Updater, MessageHandler, Filters
 
-bot = telebot.TeleBot('<341519589:AAGsM9G8_0UHiMxRF2uUhXdootK8m086Yqo>')
+TOKEN = os.environ.get('TOKEN', '341519589:AAGsM9G8_0UHiMxRF2uUhXdootK8m086Yqo')
+PORT = int(os.environ.get('PORT', '5000'))
 
-server = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+def echo(bot, update):
+    update.message.reply_text('Bot answer: ' + update.message.text)
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
 
-@server.route("/bot", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+updater = Updater(TOKEN)
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://git.heroku.com/polar-inlet-33421.git")
-    return "!", 200
+# add handlers
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-server = Flask(__name__)
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.setWebhook("https://polar-inlet-33421.herokuapp.com/" + TOKEN)
+updater.idle()
