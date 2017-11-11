a05ae0c21f3587e0a12f20745604c585cbb0a1e6
Rustem7 <30521089+Rustem7@users.noreply.github.com>
2017-11-11 14:22:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import constants
 import os
 
 
-TOKEN = constants.token
+TOKEN = "341519589:AAGsM9G8_0UHiMxRF2uUhXdootK8m086Yqo"
 PORT = int(os.environ.get('PORT', '5000'))
-updater = Updater(constants.token)
+updater = Updater(TOKEN)
 
 bot = telebot.TeleBot(constants.token)
 def parse(html):
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
-    updater.bot.set_webhook("https://polar-inlet-33421.herokuapp.com/" +constants.token)
+    updater.bot.set_webhook("https://polar-inlet-33421.herokuapp.com/" +TOKEN)
 
     
     updater.idle()
