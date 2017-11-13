b64cdf413f8e02dd007920589eca0b3c36f703ba
Rustem7 <30521089+Rustem7@users.noreply.github.com>
2017-11-14 04:59:29 +0600

    Update 123.py

--- 123.py
+++ 123.py
@@ -13,6 +13,9 @@ token = "341519589:AAGsM9G8_0UHiMxRF2uUhXdootK8m086Yqo"
 bot = telebot.TeleBot(token) 
 port = int(os.environ.get("PORT", 5000))
 
+coll = urllib.request.urlopen('http://www.kino.kz/cinema.asp?cinemaid=50')
+urll=coll.read
+
 @server.route('/')
 def webhook():
     bot.remove_webhook()
@@ -25,31 +28,26 @@ def getMessage():
         [telebot.types.Update.de_json(request.stream.read().decode("utf-8")).message])
     return "POST", 200
 
-@bot.message_handler()
-def start(message):
+@bot.message_handler(content_types=['text'])
+#def start(message):
      
-    bot.send_message(message.chat.id, 'Hi') #вот эта часть кода исполняется два или три раза
-    
-    
-def parse(html):
-    
-    @bot.message_handler(content_types=['text'])
-    def handle_text(message):
-
-        if message.text == "Фильм":
-            lines = []
-            soup = BeautifulSoup(html, 'html.parser')
-            for s in soup.find_all('div', class_='detail_content'):
-                for k in s.find_all('tr'):
-                    for b in k.find_all('strong'):
-                        z = b.text
-                        I = '🔴🎥|'
-                        lines.append(I + z + '|' + '\n' + 'Время сеанса:' + '\n' + '------------------' + '\n')
-                    for h in k.find_all('tr', class_='seance_active'):
-                        for p in h.findAll('td')[-10:1]:
-                            a = p.text[11:-5]
-                            lines.append('⏰' + a + '\n' + '------------------' + '\n')
-            bot.send_message(message.chat.id, ''.join(lines))
+ #   bot.send_message(message.chat.id, 'Hi') #вот эта часть кода исполняется два или три раза
+   
+def handle_text(message):
+    if message.text == "Фильм":
+        lines = []
+        soup = BeautifulSoup(urll, 'html.parser')
+        for s in soup.find_all('div', class_='detail_content'):
+            for k in s.find_all('tr'):
+                for b in k.find_all('strong'):
+                    z = b.text
+                    I = '🔴🎥|'
+                    lines.append(I + z + '|' + '\n' + 'Время сеанса:' + '\n' + '------------------' + '\n')
+                for h in k.find_all('tr', class_='seance_active'):
+                    for p in h.findAll('td')[-10:1]:
+                        a = p.text[11:-5]
+                        lines.append('⏰' + a + '\n' + '------------------' + '\n')
+        bot.send_message(message.chat.id, ''.join(lines))
 
 def site(url):
     coll = urllib.request.urlopen(url)
